2a3b834ebd10afafc7eba809b1fb248724e89dee
X-CYL <castellastro@gmail.com>
2022-06-22 17:39:26 +0200

    modale creating done

--- scriptJS/contact.js
+++ scriptJS/contact.js
@@ -1,16 +1,29 @@
-//place le curseur dans le premier champ de formulaire
-function focusOnFirstInput(){
-    document.getElementById('nom').focus();
-}
-//focus sur le premier champ de saisie
-let focusLine = document.getElementsByTagName('body');
-focusLine.addEventListener('load',focusOnFirstInput())
+let myModal = new bootstrap.Modal(document.getElementById("infos"), {
+  //recover the modale into myModal variable
+  keybord: true,
+});
 
-//vérifie que tous les champs sont bien remplis avant le submit
-function isEmpty(){
-let controlInput = document.getElementsByTagName('input');
-    for(let i = 0; i < controlInput.length; i++){
-        if (document.getElementsByTagName(controlInput[i]).value == "");
-            modalAlert();
+// function who will opening the modale
+function showModal() { 
+  myModal.show();
+}
+
+//put the focus at the "input" name when the page is charged
+document.body.addEventListener("load", focusOnFirstInput());
+function focusOnFirstInput() {
+  document.getElementById("nom").focus();
+}
+
+//check the inputs to see they are isn't empty
+let submitButton = document.getElementById("subBtn");
+submitButton.addEventListener("click", isEmpty);
+
+function isEmpty(form) {
+  let controlInput = document.getElementsByTagName("input");
+  for (let i = 0; i < controlInput.length; i++) {
+    if (controlInput[i].value === "") {
+      console.dir(controlInput[i]);
+      showModal();
     }
+  }
 }
